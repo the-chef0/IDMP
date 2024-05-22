ba0efdca110f64e9ab7e12c12a4a84a14fc90480
mricartoltra <82804417+mricartoltra@users.noreply.github.com>
2024-05-22 10:03:13 +0200

    Added the code to data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -1,11 +1,32 @@
-from pyepo.data import knapsack
+import numpy as np
 
-def generate_data(num_items: int):
-    # TODO: Miquel's data generation implementation
-    weights, features, values = knapsack.genData(
-        num_data=1,
-        num_features=2*num_items,
-        num_items=num_items
-    )
+# The weights are random between 3 and 8
+# The alpha values are sampled uniformly between -10 and 10
+
+def generate_data(num_items: int, data_points: int =1000, num_features: int = 1, capacity: int = 20):
+    # generate data for 1D knapsack
+    m = num_items # m number of items
+    n = data_points # n number of data AKA number of ALPHAs
+    p = num_features # p number of features (We are optimizing over alpha)
+    deg = 1 # polynomial degree
+    dim = 1 # dimension of knapsack
+
+    weights = np.array([np.round(np.random.uniform(3, 8, m),2)])
+    caps = [capacity] # capacity
+    ni = np.random.uniform(-1, 1, m)
+    ei = np.random.uniform(-1, 1, m)
+    noise = 0.5 
+    alpha = np.random.uniform(-10, 10, n)
+
+    real_values = []
+    for j in range(n):
+        data_point = []
+        for i in range(m):
+            data_point.append(np.round(np.random.normal(ni[i]*alpha[j] + ei[i], noise),0))   # Cost of the items
+            #data_point.append(np.round(np.random.normal(ni[i]*ni[i]*alpha[j] + ei[i], noise),0))   # Cost of the items
+        real_values.append(data_point)     
+
+    features = np.array(alpha).reshape((n, 1)) # Parameters, in our case Alpha
+    values = np.array(real_values)       # Cost of the items
     
     return weights, features, values
