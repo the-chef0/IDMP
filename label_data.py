a3afe6e97ac32fc0954b7d845436ddb6d660a7f5
agmercier <arthur.g.mercier@gmail.com>
2024-06-22 00:12:32 +0200

    data for shortest path

--- label_data.py
+++ label_data.py
@@ -3,19 +3,20 @@ from matplotlib.legend_handler import HandlerTuple
 import numpy as np
 from pyepo.data import dataset
 from pyepo.func import SPOPlus, perturbedFenchelYoung
-from pyepo.model.grb.knapsack import knapsackModel
 import torch
 from torch.utils.data import DataLoader
 
 from data_generator import generate_data
-from dp.dynamic import DP_Knapsack
+from dp.SP_dynamic import SP_dynamic
 from predmodel import ValueModel
-
+from pyepo.model.grb.shortestpath import shortestPathModel
+import pyepo.data.shortestpath
 from CaVEmain.src.cave import exactConeAlignedCosine
 
-experiment_size = 30
-num_items = 50
-capacity = 30
+experiment_size = 2000
+num_data = 1  # number of data
+grid = (5, 5)  # grid size
+num_feat = 2 * ((grid[0] - 1) * grid[1] + (grid[1] - 1) * grid[0])  # size of feature
 alpha_range = [-30, 30]
 alpha_values = np.arange(alpha_range[0], alpha_range[1], 0.05)
 
@@ -39,20 +40,16 @@ for exp in range(experiment_size):
     print(exp, "/", experiment_size)
     torch.manual_seed(exp)
 
-    weights, features, values = generate_data(
-        num_items=num_items, capacity=capacity, seed=exp
+    x, c = pyepo.data.shortestpath.genData(
+        num_data, num_feat, grid, deg=1, noise_width=0, seed=exp
     )
+    optmodel = shortestPathModel(grid=grid)
+    data = dataset.optDataset(model=optmodel, feats=x, costs=c)
+    dataloader = DataLoader(data, batch_size=1, shuffle=True)
 
-    optmodel = knapsackModel(weights=weights, capacity=capacity)
-    data_set = dataset.optDataset(model=optmodel, feats=features, costs=values)
-    dataloader = DataLoader(data_set, batch_size=1, shuffle=True)
-
-    # Estimate gradients with dynamic programming
-    features = features.reshape((2, num_items))
-    dp_model = DP_Knapsack(
-        weights[0], features, values, capacity, alpha_values[0], alpha_values[-1]
-    )
-    dp_model.solve()
+    x = x.reshape((2, -1))
+    sp_dynamic = SP_dynamic(x, c, grid, -5, 5)
+    sp_dynamic.solve()
 
     # label solve:
     # to -inf
@@ -63,32 +60,30 @@ for exp in range(experiment_size):
     # tiny max interval
 
     label = ""
-    y_values = dp_model.get_y_values()
-    max_value_index = np.argmax(y_values)
-    max_interval = dp_model.result.intervals[max_value_index]
-    if max_value_index == 0:
+    y_values = sp_dynamic.get_y_values()
+    min_value_index = np.argmin(y_values)
+    min_interval = sp_dynamic.result.intervals[min_value_index]
+    if min_value_index == 0:
         label = "Left"
         left_count += 1
         left_array.append(
             {
                 "label": label,
-                "num_items": num_items,
-                "capacity": capacity,
+                "grid_size": grid,
                 "seed": exp,
                 "alpha": alpha_range,
             }
         )
     elif (
-        max_value_index == len(y_values) - 1
-        or y_values[max_value_index] == y_values[-1]
+        min_value_index == len(y_values) - 1
+        or y_values[min_value_index] == y_values[-1]
     ):
         label = "Right"
         right_count += 1
         right_array.append(
             {
                 "label": label,
-                "num_items": num_items,
-                "capacity": capacity,
+                "grid_size": grid,
                 "seed": exp,
                 "alpha": alpha_range,
             }
@@ -102,34 +97,31 @@ for exp in range(experiment_size):
         middle_array.append(
             {
                 "label": label,
-                "num_items": num_items,
-                "capacity": capacity,
+                "grid_size": grid,
                 "seed": exp,
                 "alpha": alpha_range,
             }
         )
         # -1, 1
-        if max_interval[0] >= -1 and max_interval[1] <= 1:
+        if min_interval[0] >= -1 and min_interval[1] <= 1:
             label = "small_interval"
             s_inter_count += 1
             s_inter_array.append(
                 {
                     "label": label,
-                    "num_items": num_items,
-                    "capacity": capacity,
+                    "grid_size": grid,
                     "seed": exp,
                     "alpha": alpha_range,
                 }
             )
         # -4,4
-        elif max_interval[0] >= -4 and max_interval[1] <= 4:
+        elif min_interval[0] >= -4 and min_interval[1] <= 4:
             label = "medium_interval"
             m_inter_count += 1
             m_inter_array.append(
                 {
                     "label": label,
-                    "num_items": num_items,
-                    "capacity": capacity,
+                    "grid_size": grid,
                     "seed": exp,
                     "alpha": alpha_range,
                 }
@@ -141,8 +133,7 @@ for exp in range(experiment_size):
             l_inter_array.append(
                 {
                     "label": label,
-                    "num_items": num_items,
-                    "capacity": capacity,
+                    "grid_size": grid,
                     "seed": exp,
                     "alpha": alpha_range,
                 }
@@ -152,28 +143,28 @@ for exp in range(experiment_size):
         # plt.title(label)
         # plt.show()
     # tiny interval
-    if max_interval[1] - max_interval[0] < (alpha_range[1] - alpha_values[0]) * 0.01:
+    if min_interval[1] - min_interval[0] < (alpha_range[1] - alpha_values[0]) * 0.01:
         label = "tiny_interval"
         tiny_inter_count += 1
         tiny_inter_array.append(
             {
                 "label": label,
-                "num_items": num_items,
-                "capacity": capacity,
+                "grid_size": grid,
                 "seed": exp,
+                "alpha": alpha_range,
             }
         )
 
 # print("Before: ", np.array(left_array))
-np.save("labled_data\\left_labled_data.npy", left_array)
-np.save("labled_data\\right_labled_data.npy", right_array)
-np.save("labled_data\\middle_labled_data.npy", middle_array)
+np.save("labled_data\\SP_left_labled_data.npy", left_array)
+np.save("labled_data\\SP_right_labled_data.npy", right_array)
+np.save("labled_data\\SP_middle_labled_data.npy", middle_array)
 
-np.save("labled_data\\small_labled_data.npy", s_inter_array)
-np.save("labled_data\\medium_labled_data.npy", m_inter_array)
-np.save("labled_data\\large_labled_data.npy", l_inter_array)
+np.save("labled_data\\SP_small_labled_data.npy", s_inter_array)
+np.save("labled_data\\SP_medium_labled_data.npy", m_inter_array)
+np.save("labled_data\\SP_large_labled_data.npy", l_inter_array)
 
-np.save("labled_data\\tiny_labled_data.npy", tiny_inter_array)
+np.save("labled_data\\SP_tiny_labled_data.npy", tiny_inter_array)
 
 print(
     "Left count: ",
